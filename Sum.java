b2d0ba2bb097b432290ae52cdc147ebad7639f91
salah0v <asalahov17238@ada.edu.az>
2026-02-05 10:39:53 +0400

    added print for clarity

--- Sum.java
+++ Sum.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Sum {
     public static void main(String[] args)
     {
+        System.out.print("Enter 2 numbers separated by a space: ");
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
